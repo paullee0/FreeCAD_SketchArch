7ae55da891e4bacb4aa345a29a968a33791789bf
paul <40677073+paullee0@users.noreply.github.com>
2023-09-30 11:39:55 +0800

    Update UpgradeToArchSketch.py
    Script for discussion at:-
    https://forum.freecad.org/viewtopic.php?p=710317#p710315

--- Scripts/UpgradeToArchSketch.py
+++ Scripts/UpgradeToArchSketch.py
@@ -1,1 +1,10 @@
-
+import ArchSketchObject, Draft, Arch, time
+s = Gui.Selection.getSelection()[0]
+archSk = ArchSketchObject.makeArchSketch(label="ArchSketch__Layout")
+archSk.recompute()
+archSk.Placement=s.Placement
+archSk.Geometry=s.Geometry
+archSk.Constraints=s.Constraints
+archSk.recompute()
+w=s.InList[0]
+w.Base=archSk
